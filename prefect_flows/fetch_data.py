bdd7ad05c7cad7734089e12a3dd4bce93eef999f
gradedSystem <homesiknessgerald@gmail.com>
2024-10-29 18:02:15 +0700

    [add][s] Adding configurations and small update to the code

--- prefect_flows/fetch_data.py
+++ prefect_flows/fetch_data.py
@@ -1,26 +1,40 @@
 import os
+import json
 import requests
 
 from prefect import task, flow
 
 @task
-def fetch_data_from_rapidapi():
-    api_key = os.getenv("RAPID_API")  
+def fetch_query_cities():
+    source_url = 'https://github.com/lutangar/cities.json/blob/master/cities.json'
+    response = requests.get(source_url)
+    json_object = json.load(response.json())
+    return json_object
+
+@task
+def fetch_data_from_rapidapi(querystring):
+    rapid_api_key = os.getenv("RAPID_API")  
 
     # Set the endpoint and query parameters
-    url = "https://weatherapi-com.p.rapidapi.com/current.json"
-    querystring = {"q": "53.1,-0.13"} # Coordinates London just an example
+    weather_url = "https://weatherapi-com.p.rapidapi.com/current.json"
+    print(querystring)
 
     # Define the headers with the RapidAPI host and key
     headers = {
         "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
-        "x-rapidapi-key": api_key
+        "x-rapidapi-key": rapid_api_key
     }
     
-    response = requests.get(url, headers=headers, params=querystring)
-    return response.json()
+    #response = requests.get(weather_url, headers=headers, params=querystring)
+    #return response.json()
+
+# @task
+# def store_data_as_csv():
 
 @flow
 def my_pipeline():
-    data = fetch_data_from_rapidapi()
+    query_cities = fetch_query_cities()
+    for queries in query_cities:
+        data = fetch_data_from_rapidapi(queries)
+        break
     print(data)
